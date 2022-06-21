4f0af8eb6e88f1074651669bb217e9bebbec6d3b
Akhil Nolke <99820461+akhilnolke@users.noreply.github.com>
2022-06-21 20:11:12 +0530

    resolve infinte loop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const selectMenu = document.querySelectorAll("select");
 
-for (let i=12; 1 > 0; i--){
-    i = i < 10 ? "0" +  i : i;
+for (let i=12; i > 0; i--){
+    i = i < 10 ? 0 +  i : i;
     console.log(i);
 }
 
